7ca20d24a910f381b4987c49efdc470b096994d6
MissPhilbin <misscphilbin@gmail.com>
2014-02-11 10:30:25 +0000

    Space Invader Picture
    Programming Pictures in Python with Pygame.

---
+++ picture.py
@@ -0,0 +1,29 @@
+import pygame,  sys
+
+pygame.init()
+
+screen = pygame.display.set_mode([225, 150])
+
+r = pygame.Color("red")
+w = pygame.Color("white")
+
+data = [
+  [ w, w, r, r, r, r, r, w, w ],
+  [ w, w, r, w, r, w, r, w, w ],
+  [ w, w, r, r, r, r, r, w, w ],
+  [ w, r, r, r, w, r, r, r, w ],
+  [ r, w, w, r, r, r, w, w, r ],
+  [ r, r, w, w, w, w, w, r, r ]
+  ]
+
+for y, row in enumerate(data):
+  for x, colour in enumerate(row):
+    rect = pygame.Rect(x*25, y*25, 25, 25)
+    screen.fill(colour, rect=rect)
+
+pygame.display.update()
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
