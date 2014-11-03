0e6166d3226196f3f6aac420cc191b8b8d234d17
Carrie Anne Philbin <MissPhilbin@users.noreply.github.com>
2014-11-03 14:52:42 +0000

    Update spin.py

--- spin.py
+++ spin.py
@@ -1,5 +1,17 @@
-# Geek Gurl Diaries Episode 
+# Geek Gurl Diaries Episode 32
+# "Make a Digital Garden with Pibrella" by Carrie Anne Philbin
 
 import pibrella
 import time
 
+def turn():
+  while True:
+    pibrella.output.e.on()
+    pibrella.output.e.off()
+    time.sleep(0.01)
+    if not pibrella.button.read():
+      break
+
+while True:
+  if pibrella.button.read():
+    turn()
